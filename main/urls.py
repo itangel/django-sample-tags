126bf012dc6312d99d2b107179838cdeb7e8fba0
Bullish <bullish@Bullishs-Mac-mini.local>
2015-11-17 20:40:13 +0100

    finished

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, url
-from .views import IndexView
+
+from .views import IndexView, BookListView
 
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name="index"),
+    url(r'^books/$', BookListView.as_view(), name="book_list"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import CreateView, ListView
+from django.core.urlresolvers import reverse_lazy
+
+from .models import Book
 
 
-class IndexView(TemplateView):
+class IndexView(CreateView):
+	model = Book
+	success_url = reverse_lazy("book_list")
 	template_name = "main/index.html"
+	fields = ('title', 'tags')
+
+
+class BookListView(ListView):
+	model = Book
+	template_name = "main/list.html"
+	
